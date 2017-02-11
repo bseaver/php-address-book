fcb5ddee73d569a29a83f6b1694351843e6b40fc
Benjamin T. Seaver <BenjaminTSeaver@gmail.com>
2017-02-10 22:36:38 -0800

    Refactor save contact and rendor logic

--- app/app.php
+++ app/app.php
@@ -27,80 +27,99 @@
         );
     });
 
+
+    function saveAndRenderContacts($app, $edit_index, $contact)
+    {
+        $next_template = 'contacts.html.twig';
+        $entry_error = false;
+
+        // Save or Update if Contact Name and at least one other field filled in
+        if (
+            $contact->getName() &&
+            (
+                $contact->getStreetAddress() ||
+                $contact->getCityStateZip() ||
+                $contact->getPhone()
+                )
+            ) {
+            if ($edit_index == '*') {
+                $contact->save();
+                $next_template = 'create_contact.html.twig' ;
+            } else {
+                Contact::updateContact($edit_index, $contact);
+                $contact = new Contact('', '', '', '');
+                $edit_index = '*';
+            }
+        } else {
+            $entry_error = true;
+        }
+
+        if ($next_template == 'contacts.html.twig') {
+            $output = $app['twig']->render(
+                $next_template,
+                array(
+                    'edit_index' => $edit_index,
+                    'entry_error' => $entry_error,
+                    'edit_contact' => $contact,
+                    'contacts' => Contact::getAll()
+                )
+            );
+        } elseif ($next_template = 'create_contact.html.twig') {
+            $output = $app['twig']->render($next_template, array('contact' => $contact));
+        }
+
+        return $output;
+    }
+
+
     $app->post('/', function() use ($app) {
         if (array_key_exists('add_contact_button', $_POST)) {
             $contact = new Contact(
                 $_POST['contact_name'], $_POST['street_address'],
                 $_POST['city_state_zip'], $_POST['phone']
             );
-
-            // Save if Contact Name and at least one other field filled in
-            if (
-                $contact->getName() &&
-                (
-                    $contact->getStreetAddress() ||
-                    $contact->getCityStateZip() ||
-                    $contact->getPhone()
-                )
-            ) {
-                $contact->save();
-                return $app['twig']->render('create_contact.html.twig', array('contact' => $contact));
-            } else {
-                return $app['twig']->render(
-                    'contacts.html.twig',
-                    array('edit_index' => '*', 'entry_error' => true, 'edit_contact' => $contact, 'contacts' => Contact::getAll())
-                );
-            }
+            return saveAndRenderContacts($app, '*', $contact);
 
         } elseif (array_key_exists('delete_all_contacts_button', $_POST)) {
             Contact::deleteAll();
             return $app['twig']->render('delete_contacts.html.twig');
 
         } elseif (array_key_exists('delete_one_contact_button', $_POST)) {
-            $edit_contact = new Contact(
+            $contact = new Contact(
                 $_POST['contact_name'], $_POST['street_address'],
                 $_POST['city_state_zip'], $_POST['phone']
             );
             Contact::deleteOneContact($_POST['delete_one_contact_button']);
             return $app['twig']->render(
                 'contacts.html.twig',
-                array('edit_index' => '*', 'entry_error' => false, 'edit_contact' => $edit_contact, 'contacts' => Contact::getAll())
+                array(
+                    'edit_index' => '*',
+                    'entry_error' => false,
+                    'edit_contact' => $contact,
+                    'contacts' => Contact::getAll()
+                )
             );
+
         } elseif (array_key_exists('edit_one_contact_button', $_POST)) {
-            $contact_index = $_POST['edit_one_contact_button'];
-            $edit_contact = Contact::contactByIndex($contact_index);
+            $edit_index = $_POST['edit_one_contact_button'];
+            $contact = Contact::contactByIndex($edit_index);
             return $app['twig']->render(
                 'contacts.html.twig',
-                array('edit_index' => (string) $contact_index, 'entry_error' => false, 'edit_contact' => $edit_contact, 'contacts' => Contact::getAll())
+                array(
+                    'edit_index' => (string) $edit_index,
+                    'entry_error' => false,
+                    'edit_contact' => $contact,
+                    'contacts' => Contact::getAll()
+                )
             );
+
         } elseif (array_key_exists('update_contact_button', $_POST)) {
             $edit_index = $_POST['update_contact_button'];
             $contact = new Contact(
                 $_POST['contact_name'], $_POST['street_address'],
                 $_POST['city_state_zip'], $_POST['phone']
             );
-
-            // Update if Contact Name and at least one other field filled in
-            if (
-                $contact->getName() &&
-                (
-                    $contact->getStreetAddress() ||
-                    $contact->getCityStateZip() ||
-                    $contact->getPhone()
-                )
-            ) {
-                Contact::updateContact($edit_index, $contact);
-                $edit_index = '*';
-                $entry_error = false;
-                $contact = new Contact('', '', '', '');
-            } else {
-                $entry_error = true;
-            }
-
-            return $app['twig']->render(
-                'contacts.html.twig',
-                array('edit_index' => $edit_index, 'entry_error' => $entry_error, 'edit_contact' => $contact, 'contacts' => Contact::getAll())
-            );
+            return saveAndRenderContacts($app, $edit_index, $contact);
         }
     });
 
